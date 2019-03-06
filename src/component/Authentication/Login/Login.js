c88e9d5d4e61028b3da24c3f87d0c61756121b94
zoefyz12 <31747760+zoefyz12@users.noreply.github.com>
2019-03-06 09:49:18 -0800

    Part of Header Page

--- src/component/Authentication/Login/Login.js
+++ src/component/Authentication/Login/Login.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import "./Login.scss";
+
 
 const Login = () => {
     return (
         <div>
+
+            <div className= "loginbox">
+                <h1>Welcome</h1>
+                <form>
+                    <div className="inner-icon left-addon">
+                        <span className="glyphicon glyphicon-user" />
+                        <input type="text" name="username" placeholder="Enter Username" required />
+                    </div>
+                    <div className="inner-icon left-addon">
+                        <span className="glyphicon glyphicon-lock" />
+                    <input type="password" name="psw" placeholder="Enter Password" required />
+            </div>
+            <input type="submit" name="" value="Login">
+
+            </input>
+                    <a href="#">Don't have an account? </a>
+            </form>
+        </div>
 
         </div>
     );
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,9 +1,54 @@
 import React, {Component} from 'react';
-
+import Button from "react-bootstrap/Button";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import logo from "../images/ezgif-1-e382b6df9dbb.png"
+import "./Header.css"
+import Card from "react-bootstrap/Card";
 class Header extends Component {
+
 
     render() {
         return (<div>
+
+
+            <img className="rounded mx-auto d-block logo" src={logo} />
+
+            <Navbar bg="white" variant="light">
+                <Nav className="float-right">
+                    <Nav.Link href="#login">Login</Nav.Link>
+                    <Nav.Link href="#features">Shopping Cart</Nav.Link>
+                </Nav>
+            </Navbar>
+
+
+
+            <div className="vertical-menu">
+                <a href="#" className="active">View All</a>
+                <a href="#">Ink & Toner</a>
+                <a href="#">Paper & Stationery</a>
+                <a href="#">Computer & Accessories</a>
+                <a href="#">Printers & Scanners</a>
+                <a href="#">Electronics</a>
+                <a href="#">Furniture</a>
+                <a href="#">Cleaning & Facilities</a>
+            </div>
+
+            <div className="card">
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+
+
 
         </div>);
     }
